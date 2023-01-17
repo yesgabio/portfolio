e12e04177bae87bc4a51fa4520859e5065d288eb
Gabriela Oliveira <oliveirasgabi@gmail.com>
2023-01-17 14:13:58 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardOneComponent } from './components/card-one/card-one.component';
+import { CardTwoComponent } from './components/card-two/card-two.component';
+import { CardThreeComponent } from './components/card-three/card-three.component';
+import { CardFourComponent } from './components/card-four/card-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardOneComponent,
+    CardTwoComponent,
+    CardThreeComponent,
+    CardFourComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card-three/card-three.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-three',
+  templateUrl: './card-three.component.html',
+  styleUrls: ['./card-three.component.css']
+})
+export class CardThreeComponent {
+
+}
